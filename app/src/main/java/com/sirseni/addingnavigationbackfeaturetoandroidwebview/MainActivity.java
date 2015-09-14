0ce28c55cfdd5208e448bdad44fb84108f181ab4
Prem Regmi <myfrozenpark@gmail.com>
2015-09-14 21:25:17 +0545

    Initial commit

---
+++ app/src/main/java/com/sirseni/addingnavigationbackfeaturetoandroidwebview/MainActivity.java
@@ -0,0 +1,52 @@
+/* Implementing Navigation Back Feature to Android WebView */
+package com.sirseni.addingnavigationbackfeaturetoandroidwebview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.myWebView);
+        myWebView.loadUrl("http://www.centerend.com");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new MyWebViewClient());
+
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("www.centerend.com")) {
+                return false;
+            }
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack(); // Go to previous page
+            return true;
+        }
+        // Use this as else part
+        return super.onKeyDown(keyCode, event);
+    }
+}
